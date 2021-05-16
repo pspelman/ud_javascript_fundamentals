5fcbde3f8ab31819cda3f0bb4005a30c0635f22b
Philip Spelman <philip.spelman@vaporstream.com>
2021-05-16 14:51:43 -0400

    - abstracted request into getCountryData method - adjusted the attributes for the country cards - added another country

--- async_js/script.js
+++ async_js/script.js
@@ -4,14 +4,6 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
-console.log(`this is some stuff`, )
-
-// create the XML request
-const request = new XMLHttpRequest()
-
-// open it with the intended URL
-request.open('GET', 'https://restcountries.eu/rest/v2/name/portugal')
-request.send()  // note this will SEND the request, this will fetch in the background and emit the LOAD event when it is done
 
 const countryData = data => {
 	return `
@@ -21,16 +13,30 @@ const countryData = data => {
             <h3 class="country__name">${data.name}</h3>
             <h4 class="country__region">${data.region}</h4>
             <p class="country__row"><span>👫</span>${data.population}</p>
-            <p class="country__row"><span>🗣️</span>${data.languages}</p>
-            <p class="country__row"><span>💰</span>${data.currency}</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
           </div>
-        </article>`
+        </article>
+	`
 }
-// add an event listener to the request and listen for the 'load' event
-request.addEventListener('load', () => {
-	const [data] = JSON.parse(request.responseText)
-	console.log(`data: `, data)
-	const html = countryData(data)
-	countriesContainer.insertAdjacentHTML('beforeend', html)
-	countriesContainer.style.opacity = 1
-})
+
+function getCountryData(country) {
+	// create the XML request
+	const request = new XMLHttpRequest()
+
+	// open it with the intended URL
+	request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`)
+	request.send()  // note this will SEND the request, this will fetch in the background and emit the LOAD event when it is done
+
+	// add an event listener to the request and listen for the 'load' event
+	request.addEventListener('load', () => {
+		const [data] = JSON.parse(request.responseText)
+		console.log(`data: `, data)
+		const html = countryData(data)
+		countriesContainer.insertAdjacentHTML('beforeend', html)
+		countriesContainer.style.opacity = 1
+	})
+}
+
+getCountryData('usa');
+getCountryData('portugal');

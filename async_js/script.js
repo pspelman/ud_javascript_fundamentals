41181c027193c1fdd784fe6015402b3788fc1630
Philip Spelman <philip.spelman@vaporstream.com>
2021-05-16 14:46:14 -0400

    - added the template to create country cards and added the first country

--- async_js/script.js
+++ async_js/script.js
@@ -13,7 +13,24 @@ const request = new XMLHttpRequest()
 request.open('GET', 'https://restcountries.eu/rest/v2/name/portugal')
 request.send()  // note this will SEND the request, this will fetch in the background and emit the LOAD event when it is done
 
+const countryData = data => {
+	return `
+	        <article class="country">
+          <img class="country__img" src=${data.flag} />
+          <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${data.population}</p>
+            <p class="country__row"><span>🗣️</span>${data.languages}</p>
+            <p class="country__row"><span>💰</span>${data.currency}</p>
+          </div>
+        </article>`
+}
 // add an event listener to the request and listen for the 'load' event
 request.addEventListener('load', () => {
-	console.log(`res: `, request.responseText)
+	const [data] = JSON.parse(request.responseText)
+	console.log(`data: `, data)
+	const html = countryData(data)
+	countriesContainer.insertAdjacentHTML('beforeend', html)
+	countriesContainer.style.opacity = 1
 })

1ca69eed45db462d8b018beade72aa587b85ebe9
Phil Spelman <phil_spelman@hotmail.com>
2021-06-23 10:32:18 -0400

    - updated the game's callbacks to use the AbortSignal to prevent redundantly resetting event listeners when starting a new game - used the {once} option for event listeners that only need to be used once and then removed

--- dom_basics/guess_a_number/script.js
+++ dom_basics/guess_a_number/script.js
@@ -4,7 +4,9 @@ console.log(``, document.querySelector('.message').textContent)
 const gameScore = () => document.querySelector('.score')
 const guessInput = () => document.querySelector('.guess')
 
-let _score, secretNumber
+let _score, secretNumber, highScore, controller, signal
+controller = new AbortController()
+signal = controller.signal
 
 
 function updateMessage(message) {
@@ -14,17 +16,41 @@ function updateMessage(message) {
 function updateScore(delta) {
 	_score += delta
 	if (_score <= 0) {
-		updateMessage('YOU LOSE --> start a new game to try again')
-		gameScore().textContent = "0000"
-		document.querySelector('.check').textContent = 'New Game'
-		document.querySelector('.check').addEventListener('click', () => {
-			startGame(2)
-			document.querySelector('.check').textContent = 'Check!'
-		})
+		endGame(false)
 	} else {
 		gameScore().textContent = _score
 		// document.querySelector('.score').textContent = score;
 	}
+}
+
+function endGame(win) {
+	controller.abort()
+	if (win) {
+		if (!highScore) highScore = _score
+		else {
+			console.log(`checking ${_score} against high score ${highScore}`,)
+			if (_score > highScore) {
+				console.log(` new high score! `,)
+				highScore = _score
+			}
+		}
+	} else {
+		updateMessage('YOU LOSE --> start a new game to try again')
+
+		gameScore().textContent = "0000"
+	}
+	controller = new AbortController()
+	signal = controller.signal
+
+	guessInput().value = ''
+	document.querySelector('.check').textContent = 'New Game'
+	document.querySelector('.check').addEventListener('click', () => {
+			startGame(2)
+		}, {once: true}
+	)
+	guessInput().addEventListener('keypress', (e) => e.key === 'Enter' ? startGame() : null,
+		{once: true})
+
 }
 
 
@@ -36,18 +62,24 @@ function startGame(startingPoints = 2) {
 
 	// start by generating the target number 1-20
 	secretNumber = Math.trunc(Math.random() * 20) + 1
-	document.querySelector('.check').addEventListener('click', onGuessCheck)
+	guessInput().addEventListener('keypress', (e) => e.key === 'Enter' ? onGuessCheck() : null,
+		{signal})
+	document.querySelector('.check').addEventListener('click', onGuessCheck,
+		{signal})
 	guessInput().value = ''
-	guessInput().addEventListener('keypress', (e) => e.key === 'Enter' ? onGuessCheck() : null)
+	document.querySelector('.check').textContent = 'Check!'
 }
 
 let onGuessCheck = function () {
-	const guess = Number(document.querySelector('.guess').value)
+	let guess = Number(document.querySelector('.guess').value)
+	console.log(`checking guess: `, guess)
 	if (!guess) {
 		updateMessage('Guess a NUMBER!');
 	} else if (guess === secretNumber) {
+		console.log(`YOU WIN`,)
 		updateMessage('Correct Number!')
 		updateScore(+1)
+		endGame(true)
 	} else if (guess > secretNumber) {
 		updateMessage('Too High')
 		updateScore(-1)
